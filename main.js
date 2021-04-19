b9bb7af8dc09e9ada9822c64f07b4e6f2fb123d9
Stuwar Newmann <newmannsoft@gmail.com>
2021-04-19 05:28:57 -0500

    minusculas

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ window.addEventListener('load', () => {
     /* Filtrado de imagenes Barra de busqueda */
 
     document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
-        const busqueda = evento.target.value;
+        const busqueda = evento.target.value.toLowerCase();
         grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda));
     });
 
