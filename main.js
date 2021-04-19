82b47946681ea4dac465a298205efc04da8701fb
Stuwar Newmann <newmannsoft@gmail.com>
2021-04-18 23:48:35 -0500

    filtrado js

--- main.js
+++ main.js
@@ -3,8 +3,22 @@ const grid = new Muuri('.grid', {
         rounding: false
     }
 });
-
+/*trancition*/
 window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
+    /*Selector */
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'))
+            evento.target.classList.add('activo');
+            /*Selector y conversor a minusculas */
+            const categoria = evento.target.innerHTML.toLowerCase();
+            /*Filtrado */
+            grid.filter('[data-categoria="categoria"]');
+        });
+    });
 });
+
